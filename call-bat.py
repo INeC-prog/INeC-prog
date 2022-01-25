b54d59b66b235cf8d937dac662fa7a5443196e8f
INeC-prog <84381493+INeC-prog@users.noreply.github.com>
2022-01-25 19:20:25 -0300

    Call-bat: batch file. D: Familiarization task. A: Control. A10: low fear (80dB). A100: high fear (85dB). A1000: context only (same background as high fear but without white noise. AA: last control, session similar to A (blue background)igh
    Anxiety disorders are a group of disorders that spam from generalized anxiety to post traumatic stress disorder (PTSD). People with anxiety display an array of symptoms encompassing emotional processing, reactions to moderate to intense fear and autonomic alterations.  Beyond questionnaires or interviews the psychiatry clinic does not have a simple and effective evaluation of the individual susceptibility or propensity to develop anxiety disorder through objective behavioral and autonomic changes triggered by potential or proximal fear stimuli. Here, we developed and validated an active prevention virtual reality paradigm (VRP) to study a threat’s spatial imminence using a dynamic threat-of-loud noise paradigm. Delineated on the Darwinian concept of prey/predator validated in animal models of anxiety, particularly on the processing of aversive stimuli of acoustic nature, it is proposed how stressful conditions could produce behavioral and autonomic changes characteristic of anxiety disorders. The dystopic functioning circuit responsible for processing aversive stimuli will eventually come to dominate the individual reactivity to incoming fear stimuli contributing to the development of anxiety disorders.

---
+++ call-bat.py
@@ -0,0 +1,15 @@
+from subprocess import call
+call(["python", "D.py"]),
+call(["python", "black_screen_1.py"]),
+call(["python", "A.py"]),
+call(["python", "black_screen_1.py"]),
+call(["python", "A10.py"]),
+call(["python", "black_screen_1.py"]),
+call(["python", "A100.py"]),
+call(["python", "black_screen_1.py"]),
+call(["python", "A1000.py"]),
+call(["python", "black_screen_1.py"]),
+call(["python", "AA.py"])
+
+if __name__ == '__main__':
+    print('End')
